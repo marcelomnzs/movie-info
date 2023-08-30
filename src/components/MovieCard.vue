ba1f32bea094f7667ae071694d644145bdb12b1c
Marcelo Antonio <inho.menezes2011@gmail.com>
2023-08-30 08:15:07 -0300

    Implementing API call for the goat page

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path">
+        <h3>{{ movie.title }}</h3>
+    </div>
+</template>
+
+<script>
+import { useMovieStore } from '../stores/MoviesStore';
+
+    export default {
+        name: 'MovieCard',
+        props: ['movie'],
+
+        setup() {
+            const movieStore = useMovieStore()
+            return { movieStore }
+        }
+    }
+</script>
