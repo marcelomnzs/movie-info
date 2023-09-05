0a578a815abca25b6d511591ef482b691b06005d
Marcelo Antonio <inho.menezes2011@gmail.com>
2023-09-04 21:32:18 -0300

    Refactoring the component Moviecard to allow the exhibition of different types of mediaType

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import { ref } from 'vue'
-// import { useMovieStore } from './stores/MoviesStore'
 import NavBar from './components/NavBar.vue'
 import ToWatchForm from './components/ToWatchForm.vue'
-// import ToWatchMoviesDetails from './components/ToWatchMoviesDetails.vue'
-// import { useToWatchStore } from './stores/ToWatchStore'
 const inputValue = ref('')
-    // const movieStore = useMovieStore()
-    // const toWatchStore = useToWatchStore()
 
 </script>
 
@@ -20,19 +15,7 @@ const inputValue = ref('')
     <main>
         <div v-if="$router.currentRoute.value.name != 'search'">
             <ToWatchForm v-model="inputValue"/>
-        </div>
-
-        <!-- <div class="mt-3 p-5">
-            <div v-for="item in movieStore.searchResult" :key="item.id">
-                <ToWatchMoviesDetails :item="item" />
-            </div>
-        </div>
-
-        <div class="mt-3 p-5 bg-red-600">
-            <div v-for="item in toWatchStore.toWatchList" :key="item.id">
-                <ToWatchMoviesDetails :item="item" />
-            </div>
-        </div> -->
+        </div> 
     </main>
     <RouterView />
 </template>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,35 +1,38 @@
 <template>
-    <div class="flex items-center justify-center">
+    <!-- <div class="flex items-center justify-center">
         {{ movieStore.searchResult }}
+    </div> -->
+    <div class="grid grid-cols-4 px-12 py-8 gap-7">
+        <div v-for="item in movieStore.searchResult" :key="item.id">
+            <MovieCard :movie="item" :mediaType="mediaType"/>
+        </div>
     </div>
 </template>
 
 <script>
 import { useRoute } from 'vue-router';
 import { useMovieStore } from '../stores/MoviesStore';
+import MovieCard from '../components/MovieCard.vue';
 
 export default {
     name: 'SearchPage',
     setup() {
-        const router = useRoute()
-        const movieStore = useMovieStore()
+        const router = useRoute();
+        const movieStore = useMovieStore();
+        const mediaType = router.query.media;
         // Função disparada no envio do form
         const handleSubmit = () => {
-            const data = router.params.item
-            const searchType = router.query.media    
-
+            const data = router.params.item;
             // Verifies if the input is empty
             if (data.length > 0) {
-                movieStore.search(data, searchType);
+                movieStore.search(data, mediaType);
             }
         };
-        return { handleSubmit, router, movieStore }
-
+        return { handleSubmit, router, movieStore, mediaType };
     },
-
     mounted() {
-        this.handleSubmit()
-    }
-
+        this.handleSubmit();
+    },
+    components: { MovieCard }
 }
 </script>
